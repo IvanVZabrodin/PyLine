5062d766806ad3bd64d1bf5bbd28aaaf97d81189
Ivan Zabrodin <ivanvzabrodin@outlook.com>
2023-04-04 19:46:18 +0100

    Getting rid of __pychache__ 5

--- src/terminal.py
+++ src/terminal.py
@@ -4,5 +4,7 @@ system("title PyLine: Loading...")
 
 
 class Terminal (Line):
-    def __init__(self, name: str):
+    def __init__(self, name: str, lform: LineFormat | tuple[str, str, int] | tuple[str, str, int, int] = LineFormat()):
+        super(lform)
         self.name = name
+
